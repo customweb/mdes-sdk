12686bb352c46edc412fbc67a49dd6133cfbc57e
rodriguez <elier.rodriguez@customweb.com>
2019-10-04 20:11:57 +0200

    Release 1.1.4

--- before_deploy.sh
+++ before_deploy.sh
@@ -6,7 +6,6 @@ then
 
 else
 	echo "[INFO] Running before_deploy script. Decrypting 'codesigning.asc.enc'..."
-	
 	openssl aes-256-cbc -K $encrypted_5d3bf8e2f4dc_key -iv $encrypted_5d3bf8e2f4dc_iv -in codesigning.asc.enc -out codesigning.asc -d && \
 	gpg --fast-import codesigning.asc;
 fi
