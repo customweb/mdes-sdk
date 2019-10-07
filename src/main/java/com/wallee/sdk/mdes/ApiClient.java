a4ca95b16ac54bc6b7b6b8caf8898f9a13f9ba44
rodriguez <elier.rodriguez@customweb.com>
2019-10-07 11:43:21 +0200

    Release 1.1.11

--- src/main/java/com/wallee/sdk/mdes/ApiClient.java
+++ src/main/java/com/wallee/sdk/mdes/ApiClient.java
@@ -237,7 +237,7 @@ public class ApiClient {
         this.lenientDatetimeFormat = true;
 
         // Set default User-Agent.
-        setUserAgent("Swagger-Codegen/1.1.10/java");
+        setUserAgent("Swagger-Codegen/1.1.11/java");
 
         // Setup authentications (key: authentication name, value: authentication).
         authentications = new HashMap<String, Authentication>();
