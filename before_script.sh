3cc5772a11541dd5d54192bbdc4dd5f9c27ae481
rodriguez <elier.rodriguez@customweb.com>
2019-10-07 15:05:21 +0200

    Release 1.1.15

---
+++ before_script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "${TRAVIS_TAG}" ]; 
+then
+	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping before_deploy step.";
+
+else
+	echo "[INFO] Running before_script script. Decrypting 'wallee_M4M-sandbox.p12.enc', 'public-key-encrypt.crt.enc', 'private-key-decrypt.pem.enc'"
+	
+	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in wallee_M4M-sandbox.p12.enc -out wallee_M4M-sandbox.p12 -d
+	
+	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in public-key-encrypt.crt.enc -out public-key-encrypt.crt -d
+	
+    openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in private-key-decrypt.pem.enc -out private-key-decrypt.pem -d
+	
+fi
