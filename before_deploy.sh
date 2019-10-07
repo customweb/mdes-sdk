a5c03fdb932bcd22fd6bbe3555eb4449da8e3516
rodriguez <elier.rodriguez@customweb.com>
2019-10-07 15:36:20 +0200

    Release 1.1.18

--- before_deploy.sh
+++ before_deploy.sh
@@ -8,11 +8,4 @@ else
 	echo "[INFO] Running before_deploy script. Decrypting 'codesigning.asc.enc'..."
 	openssl aes-256-cbc -K $encrypted_5d3bf8e2f4dc_key -iv $encrypted_5d3bf8e2f4dc_iv -in codesigning.asc.enc -out codesigning.asc -d && \
 	gpg --fast-import codesigning.asc;
-	
-	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in wallee_M4M-sandbox.p12.enc -out wallee_M4M-sandbox.p12 -d
-	
-	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in public-key-encrypt.crt.enc -out public-key-encrypt.crt -d
-	
-    openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in private-key-decrypt.pem.enc -out private-key-decrypt.pem -d
-	
 fi
--- before_script.sh
+++ before_script.sh
@@ -7,9 +7,9 @@ then
 else
 	echo "[INFO] Running before_script script. Decrypting 'wallee_M4M-sandbox.p12.enc', 'public-key-encrypt.crt.enc', 'private-key-decrypt.pem.enc'"
 	
-	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/wallee_M4M-sandbox.p12.enc -out ./src/test/resources/wallee_M4M-sandbox.p12 -d
+	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/wallee_M4M-sandbox.p12.enc -out ./src/test/resources/wallee_M4M-sandbox.p12 -d  && \
 	
-	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/public-key-encrypt.crt.enc -out ./src/test/resources/public-key-encrypt.crt -d
+	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/public-key-encrypt.crt.enc -out ./src/test/resources/public-key-encrypt.crt -d  && \
 	
     openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d
 	
