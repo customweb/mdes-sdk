8d0e45f1b6afd3a180b4d23367a2504b50e92a14
rodriguez <elier.rodriguez@customweb.com>
2019-10-08 11:18:50 +0200

    Release 1.1.0

--- before_deploy.sh
+++ before_deploy.sh
@@ -5,6 +5,6 @@ then
 	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping before_deploy step.";
 else
 	echo "[INFO] Running before_deploy script. Decrypting 'codesigning.asc.enc'..."
-	openssl aes-256-cbc -K $encrypted_26bc6e8afb91_key -iv $encrypted_26bc6e8afb91_iv -in codesigning.asc.enc -out codesigning.asc -d && \
+	openssl aes-256-cbc -K $encrypted_5d3bf8e2f4dc_key -iv $encrypted_5d3bf8e2f4dc_iv -in codesigning.asc.enc -out codesigning.asc -d && \
 	gpg --fast-import codesigning.asc;
 fi
