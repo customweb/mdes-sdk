035a77cdfa63344dc6d9ca277241e8efbc17f326
rodriguez <elier.rodriguez@customweb.com>
2019-10-08 19:12:45 +0200

    Release 1.1.1

--- before_script.sh
+++ before_script.sh
@@ -8,4 +8,5 @@ else
     openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/wallee_M4M-sandbox.p12.enc -out ./src/test/resources/wallee_M4M-sandbox.p12 -d;
 	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/public-key-encrypt.crt.enc -out ./src/test/resources/public-key-encrypt.crt -d;
 	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d;
+	ls ./src/test/resources;	
 fi
