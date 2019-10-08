ec90f5a1b9560c2529d7fa78a0cf981a96738ee7
rodriguez <elier.rodriguez@customweb.com>
2019-10-08 18:44:24 +0200

    Release 1.1.1

--- before_script.sh
+++ before_script.sh
@@ -5,7 +5,8 @@ then
 	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping before_script step.";
 else
 	echo "[INFO] Running before_script script. Decrypting 'wallee_M4M-sandbox.p12.enc', 'public-key-encrypt.crt.enc', 'private-key-decrypt.pem.enc'";
-	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in src/test/resources/wallee_M4M-sandbox.p12.enc -out src/test/resources/wallee_M4M-sandbox.p12 -d ;# && \
+	ls ./src/test;
+	openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/wallee_M4M-sandbox.p12.enc -out src/test/resources/wallee_M4M-sandbox.p12 -d ;# && \
 	#openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/public-key-encrypt.crt.enc -out ./src/test/resources/public-key-encrypt.crt -d  && \
     #openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d;
 fi
