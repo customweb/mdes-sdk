96402c226f5640a2e240009aa12b032d0572025e
rodriguez <elier.rodriguez@customweb.com>
2019-10-10 16:14:23 +0200

    Release 1.1.1

--- before_install.sh
+++ before_install.sh
@@ -2,4 +2,4 @@
 
 echo "[INFO] Running before_script script. Decrypting 'wallee_M4M-sandbox.p12.enc', 'public-key-encrypt.crt.enc', 'private-key-decrypt.pem.enc'";	
 # the private key should be load before-install, because it is required for running the junit
-openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d
+openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d;
--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,11 @@ then
 	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping deployment.";
 else
 	echo "[INFO] Running deployment based on the mvn_settings.xml"
-	mvn deploy -e -P sign,build-extras --settings mvn_settings.xml
+	mvn deploy -e -P sign,build-extras --settings mvn_settings.xml;
+	
+	if [ $? -ne 0 ]
+	then
+	  echo "[ERROR] Deployment failed with exit code $?."
+	  exit 1;
+	fi	
 fi
