18dc8df7f08cb716d83f3d41b3262d70976b4411
rodriguez <elier.rodriguez@customweb.com>
2019-10-10 15:57:48 +0200

    Release 1.1.1

--- before_install.sh
+++ before_install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "${TRAVIS_TAG}" ]; 
-then
-	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping before_script step.";
-else
-	echo "[INFO] Running before_script script. Decrypting 'wallee_M4M-sandbox.p12.enc', 'public-key-encrypt.crt.enc', 'private-key-decrypt.pem.enc'";	
-	# the private key should be load before-install, because it is required for running the junit
-    openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d;
-fi
+echo "[INFO] Running before_script script. Decrypting 'wallee_M4M-sandbox.p12.enc', 'public-key-encrypt.crt.enc', 'private-key-decrypt.pem.enc'";	
+# the private key should be load before-install, because it is required for running the junit
+openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/private-key-decrypt.pem.enc -out ./src/test/resources/private-key-decrypt.pem -d
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 if [ -z "${TRAVIS_TAG}" ]; 
 then
 	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping deployment.";
-
 else
 	echo "[INFO] Running deployment based on the mvn_settings.xml"
 	mvn deploy -e -P sign,build-extras --settings mvn_settings.xml
