ab0c18f84df63d16baee0f3a770812070d670eea
rodriguez <elier.rodriguez@customweb.com>
2019-10-10 17:27:20 +0200

    Release 1.1.1

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,6 @@ then
 else
 	echo "[INFO] Running deployment based on the mvn_settings.xml"
 	mvn deploy -e -P sign,build-extras --settings mvn_settings.xml;
-	
-	if [ $? -eq 0 ]
-	then
-		exit 0;
-	else
-	  echo "[ERROR] Deployment failed with exit code $?."
-	  exit 1;
-	fi	
+	exit 1;
+	# TODO even returning "exit 1;" Travis does not abort the build, and eventually it is mark as "passing".
 fi
