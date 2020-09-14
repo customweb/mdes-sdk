d98c731cc0ec85510bcdee1bc1e76069e5ac75a8
elrocw <elier.rodriguez@wallee.com>
2020-09-14 16:03:33 +0200

    Release 1.1.17

--- before_install.sh
+++ before_install.sh
@@ -2,4 +2,4 @@
 
 echo "[INFO] Running before_install script. Decrypting private key."
 # the private key should be load before-install, because it is required for running the junit
-openssl aes-256-cbc -K $encrypted_a09fc3b71c0b_key -iv $encrypted_a09fc3b71c0b_iv -in ./src/test/resources/e5fec5dc4e2fab3c968dbe78a905Private-Key-Decrypt.pem.enc -out ./src/test/resources/e5fec5dc4e2fab3c968dbe78a905Private-Key-Decrypt.pem -d;
+openssl aes-256-cbc -K $encrypted_ec3b1dbe57ae_key -iv $encrypted_ec3b1dbe57ae_iv -in ./src/test/resources/e5fec5dc4e2fab3c968dbe78a905Private-Key-Decrypt.pem.enc -out ./src/test/resources/e5fec5dc4e2fab3c968dbe78a905Private-Key-Decrypt.pem -d;
